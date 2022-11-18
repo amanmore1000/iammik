691da3b2bbbd3cf3a5f364285e81120023d96e5d
amanmore1000 <72276618+amanmore1000@users.noreply.github.com>
2022-11-18 16:58:02 +0530

    Update contact-form-process.php

--- contact-form-process.php
+++ contact-form-process.php
@@ -1,76 +1,60 @@
-<?php
-if (isset($_POST['Email'])) {
 
-    // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "amanmore1000@gmail.com";
-    $email_subject = "New form submissions";
+<?php 
+if(isset($_POST['submit'])){
+    $to = "amanmore1000@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Emailaddress 
+    
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $pnumber = $_POST['pnumber'];
 
-    function problem($error)
-    {
-        echo "We're sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br><br>";
-        echo $error . "<br><br>";
-        echo "Please go back and fix these errors.<br><br>";
-        die();
+
+   
+
+   
+
+
+    $subject = "New form submissions";
+   
+
+$headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+        $message = '<html><body>';
+        $message .= '<table border="1">
+        <tr>
+      
+          <th>fname</th> 
+          <th>lname</th>
+          <th>email</th> 
+          <th>pnumber</th> 
+  
+         
+          
+        </tr>
+        <tr>
+          
+          <td>'.$fname.'</td>
+          <td>'.$lname.'</td>
+          <td>'.$email.'</td>
+          <td>'.$pnumber.'</td>
+
+    
+      
+         
+
+        </tr>
+      </table>';
+
+
+   
+
+   
+    mail($to,$subject,$message,implode("\r\n",$headers));
+  
+   
+
+     header('Location: ""add your redirection path"");
+    
     }
-
-    // validation expected data exists
-    if (
-        !isset($_POST['Name']) ||
-        !isset($_POST['Email']) ||
-        !isset($_POST['Message'])
-    ) {
-        problem("We're sorry, but there appears to be a problem with the form you submitted.");
-    }
-
-    $name = $_POST['Name']; // required
-    $email = $_POST['Email']; // required
-    $message = $_POST['Message']; // required
-
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-    if (!preg_match($email_exp, $email)) {
-        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
-    }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-    if (!preg_match($string_exp, $name)) {
-        $error_message .= 'The Name you entered does not appear to be valid.<br>';
-    }
-
-    if (strlen($message) < 2) {
-        $error_message .= 'The Message you entered do not appear to be valid.<br>';
-    }
-
-    if (strlen($error_message) > 0) {
-        problem($error_message);
-    }
-
-    $email_message = "Form details below.\n\n";
-
-    function clean_string($string)
-    {
-        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-        return str_replace($bad, "", $string);
-    }
-
-    $email_message .= "Name: " . clean_string($name) . "\n";
-    $email_message .= "Email: " . clean_string($email) . "\n";
-    $email_message .= "Message: " . clean_string($message) . "\n";
-
-    // create email headers
-    $headers = 'From: ' . $email . "\r\n" .
-        'Reply-To: ' . $email . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $email_subject, $email_message, $headers);
-?>
-
-    <!-- INCLUDE YOUR SUCCESS MESSAGE BELOW -->
-
-    Thanks for getting in touch. We'll get back to you soon.
-
-<?php
-}
 ?>
